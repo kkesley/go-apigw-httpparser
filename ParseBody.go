c57ce825893fc7325c73bbf8673122745e50b5d1
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-06-01 09:43:44 +1000

    parse body

---
+++ ParseBody.go
@@ -0,0 +1,21 @@
+package httpparser
+
+import (
+	"net/http/httptest"
+	"strings"
+
+	"github.com/albrow/forms"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+//ParseBody parses APIGatewayProxyRequest object to forms.Data
+func ParseBody(request events.APIGatewayProxyRequest) (*forms.Data, error) {
+	httpRequest := httptest.NewRequest(request.HTTPMethod, request.Path, strings.NewReader(request.Body))
+	httpRequest.Header.Add("Content-Type", request.Headers["Content-Type"])
+	err := httpRequest.ParseForm()
+	if err != nil {
+		return nil, err
+	}
+	userData, err := forms.Parse(httpRequest)
+	return userData, err
+}
