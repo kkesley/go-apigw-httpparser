d53ed24fefbf4d3d66edc9a64cf72db8a182cecb
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-06-06 13:56:54 +1000

    what

--- ParseBody.go
+++ ParseBody.go
@@ -9,18 +9,21 @@ import (
 )
 
 //ParseBody parses APIGatewayProxyRequest object to forms.Data
-func ParseBody(request events.APIGatewayProxyRequest) (*forms.Data, error) {
+func ParseBody(request events.APIGatewayProxyRequest) (*forms.Data, error, string) {
 	httpRequest := httptest.NewRequest(request.HTTPMethod, request.Path, strings.NewReader(request.Body))
+	c := "abc"
 	if content, ok := request.Headers["Content-Type"]; ok {
 		httpRequest.Header.Set("Content-Type", content)
+		c = content
 	} else if content, ok := request.Headers["content-type"]; ok {
 		httpRequest.Header.Set("Content-Type", content)
+		c = content
 	}
 
 	err := httpRequest.ParseForm()
 	if err != nil {
-		return nil, err
+		return nil, err, ""
 	}
 	userData, err := forms.Parse(httpRequest)
-	return userData, err
+	return userData, err, c
 }
