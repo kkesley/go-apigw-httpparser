8ab8a23f2e5c00d108ca5da79a146870737943a3
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-23 13:35:40 +1000

    add test

---
+++ ParseBody_test.go
@@ -0,0 +1,23 @@
+package httpparser
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestParseBody(test *testing.T) {
+	request := events.APIGatewayProxyRequest{
+		HTTPMethod: "POST",
+		Resource:   "hello",
+		Path:       "/hello",
+		Headers:    map[string]string{"content-type": "application/json"},
+		Body:       "{\"hello\": \"there\"}",
+	}
+	data, err := ParseBody(request)
+	if err != nil {
+		test.Fail()
+	}
+	fmt.Println(data)
+}
