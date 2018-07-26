4f063b51972524a96201795d54b69f7939576473
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-26 15:13:57 +1000

    don't return nil

--- ParseBody.go
+++ ParseBody.go
@@ -12,7 +12,7 @@ import (
 //ParseBody parses APIGatewayProxyRequest object to forms.Data
 func ParseBody(request events.APIGatewayProxyRequest) (*forms.Data, error) {
 	if len(request.Path) <= 0 || len(request.HTTPMethod) <= 0 {
-		return nil, errors.New("invalid body request")
+		return forms.CreateFromMap(make(map[string]string)), errors.New("invalid body request")
 	}
 	httpRequest := httptest.NewRequest(request.HTTPMethod, request.Path, strings.NewReader(request.Body))
 	if content, ok := request.Headers["Content-Type"]; ok {
