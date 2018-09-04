d77ff12a390395f9965019b7be0e7c6f29ef9854
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-04 12:29:24 +1000

    log request

---
+++ LogRequest.go
@@ -0,0 +1,17 @@
+package httpparser
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+//LogRequest logs request to stderr
+func LogRequest(request interface{}) {
+	b, err := json.Marshal(request)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	fmt.Fprintf(os.Stderr, "REQUEST: %s\n", string(b))
+}
