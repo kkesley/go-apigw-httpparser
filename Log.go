7db761aca05f0f63c6052cc023dd8abaf4db61d6
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-19 10:28:33 +1100

    log

--- Log.go
+++ Log.go
@@ -11,35 +11,16 @@ import (
 
 //LogRequest logs request to stderr
 func LogRequest(request interface{}) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			if _, ok := r.(runtime.Error); ok {
-				panic(r)
-			}
-			if s, ok := r.(string); ok {
-				panic(s)
-			}
-			err = r.(error)
-		}
-	}()
-	value := reflect.ValueOf(request)
-	if value.Kind() != reflect.Ptr || value.IsNil() {
-		return errors.New("value must be pointer")
-	}
-	if err := stripValues(reflect.TypeOf(request).Elem(), value.Elem()); err != nil {
-		return nil
-	}
-	b, err := json.Marshal(request)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-		return nil
-	}
-	fmt.Fprintf(os.Stderr, "REQUEST: %s\n", string(b))
-	return nil
+	return Log(request, "REQUEST")
 }
 
 //LogResponse log a response
 func LogResponse(code int, request interface{}) (err error) {
+	return Log(request, fmt.Sprintf("RESPONSE-%d", code))
+}
+
+//Log the request
+func Log(request interface{}, logType string) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			if _, ok := r.(runtime.Error); ok {
@@ -63,7 +44,7 @@ func LogResponse(code int, request interface{}) (err error) {
 		fmt.Printf("Error: %s", err)
 		return nil
 	}
-	fmt.Fprintf(os.Stderr, "RESPONSE-%d: %s\n", code, string(b))
+	fmt.Fprintf(os.Stderr, logType+": %s\n", string(b))
 	return nil
 }
 
