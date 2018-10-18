1dbd3c1ddaf730487eca4b3b39ac0c40d5730284
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-19 09:59:11 +1100

    log response

--- Log.go
+++ Log.go
@@ -11,11 +11,6 @@ import (
 
 //LogRequest logs request to stderr
 func LogRequest(request interface{}) (err error) {
-	return Log(request, "REQUEST")
-}
-
-//Log the object
-func Log(message interface{}, logType string) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			if _, ok := r.(runtime.Error); ok {
@@ -27,25 +22,49 @@ func Log(message interface{}, logType string) (err error) {
 			err = r.(error)
 		}
 	}()
-	value := reflect.ValueOf(message)
+	value := reflect.ValueOf(request)
 	if value.Kind() != reflect.Ptr || value.IsNil() {
 		return errors.New("value must be pointer")
 	}
-	if err := stripValues(reflect.TypeOf(message).Elem(), value.Elem()); err != nil {
+	if err := stripValues(reflect.TypeOf(request).Elem(), value.Elem()); err != nil {
 		return nil
 	}
-	b, err := json.Marshal(message)
+	b, err := json.Marshal(request)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 		return nil
 	}
-	fmt.Fprintf(os.Stderr, logType+": %s\n", string(b))
+	fmt.Fprintf(os.Stderr, "REQUEST: %s\n", string(b))
 	return nil
 }
 
 //LogResponse log a response
-func LogResponse(request interface{}) (err error) {
-	return Log(request, "RESPONSE")
+func LogResponse(code int, request interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			if s, ok := r.(string); ok {
+				panic(s)
+			}
+			err = r.(error)
+		}
+	}()
+	value := reflect.ValueOf(request)
+	if value.Kind() != reflect.Ptr || value.IsNil() {
+		return errors.New("value must be pointer")
+	}
+	if err := stripValues(reflect.TypeOf(request).Elem(), value.Elem()); err != nil {
+		return nil
+	}
+	b, err := json.Marshal(request)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return nil
+	}
+	fmt.Fprintf(os.Stderr, "RESPONSE-%d: %s\n", code, string(b))
+	return nil
 }
 
 func stripValues(t reflect.Type, v reflect.Value) (err error) {
